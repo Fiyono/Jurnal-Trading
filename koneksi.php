97752e6fe436706a9de52e7e83cd1954ede20f29
Fiyono <fiyono25696@gmail.com>
2026-06-03 12:03:36 +0700

    ignore koneksi.php

--- koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$koneksi = mysqli_connect("sql301.infinityfree.com", "if0_41421698", "JtWEmypwMgGnPL", "if0_41421698_jurnal_trading"); 
-
-if (!$koneksi) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
-?>
